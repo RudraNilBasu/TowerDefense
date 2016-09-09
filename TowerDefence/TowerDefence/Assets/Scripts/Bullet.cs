1d72645199cc800534f527ab052a8e3d19c018ae
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2016-09-09 14:52:56 +0530

    Missile Explosion

--- TowerDefence/TowerDefence/Assets/Scripts/Bullet.cs
+++ TowerDefence/TowerDefence/Assets/Scripts/Bullet.cs
@@ -1,11 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Bullet : MonoBehaviour {
 
 	private Transform target;
 	public float speed = 70f;
-
+    public float explosionRadius = 0f;
 	public GameObject impactEffect;
 
 	public void Seek(Transform _target)
@@ -15,7 +15,7 @@ public class Bullet : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (target == null) {
-			Destroy (target);
+			Destroy (gameObject);
 			return;
 		}
 
@@ -28,6 +28,7 @@ public class Bullet : MonoBehaviour {
 		}
 
 		transform.Translate (dir.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
 	}
 
 	void HitTarget()
@@ -35,7 +36,36 @@ public class Bullet : MonoBehaviour {
 		GameObject effectIns =  (GameObject)Instantiate (impactEffect, transform.position, transform.rotation);
 		Destroy (effectIns, 2f);
 
-		Destroy (target.gameObject);
+        if (explosionRadius > 0f)
+        {
+            Explode();
+        }
+        else {
+            Damage(target);
+        }
+		
 		Destroy (gameObject);
 	}
+
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.tag == "Enemy") {
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    void Damage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
 }
